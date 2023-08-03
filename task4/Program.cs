b1ccb0c5193f436a076b6fc1dd17cbd67cb36d3f
DmitPloh <kroll.88@mail.ru>
2023-08-03 15:24:10 +0300

    complete task4

---
+++ task4/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int i = 1;
+while (i <= number)
+{
+    if (i % 2 == 0)
+    {
+        Console.Write(i + "; ");
+    }
+    i += 1;
+}
