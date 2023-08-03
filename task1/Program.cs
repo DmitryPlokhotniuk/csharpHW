ef4a1a38632378b36cd86aa165199a56204183f1
DmitPloh <kroll.88@mail.ru>
2023-08-03 15:03:14 +0300

    complete task1

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,12 @@
 ﻿Console.WriteLine("Введите 2 числа: ");
 int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 > number2)
+{
+    Console.WriteLine(number1 + " > " + number2);
+}
+else
+{
+    Console.WriteLine(number2 + " > " + number1);
+}
